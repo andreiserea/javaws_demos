ac326ae5052c417bcbee98cd2e3330d6f38eb459
Andrei Serea <aserea@fitbit.com>
2020-03-10 10:36:24 +0200

    UserFileDaoTest.test_Delete without mockito

--- demo.ticketmonster/src/test/java/com/aserea/dao/UserFileDaoTest.java
+++ demo.ticketmonster/src/test/java/com/aserea/dao/UserFileDaoTest.java
@@ -2,7 +2,12 @@ package com.aserea.dao;
 
 import com.aserea.exceptions.EntityNotFoundException;
 import com.aserea.model.User;
+import com.aserea.storage.Query;
+import com.aserea.storage.StorageEngineConnection;
+import com.aserea.storage.file.FileQuery;
+import com.aserea.storage.file.FileStorageConnection;
 import com.aserea.storage.file.FileStorageEngine;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,23 +15,46 @@ import java.util.List;
 
 public class UserFileDaoTest {
 
-    @Test(expected = EntityNotFoundException.class)
+    @Test
     /**
-     * This test is wrong because it actually does not isolate methods.
-     * This test can fail because of any number of bugs in multiple methods: UserFileDao.delete/UserFileDao.get,
-     * FileStorageEngine or FileQuery
+     * Good Test only tests UserFileDao.delete method in isolation.
      */
     public void test_deleteUser() {
-        List<User> users = Arrays.asList(
-                new User(1, "andrei", "aserea@fitbit.com", "Andrei", "Serea", "Home"),
-                new User(2, "ion", "ion@fitbit.com", "Ion", "Popescu", "Home")
-        );
-        FileStorageEngine fileStorageEngine = new FileStorageEngine(".");
-        UserFileDao userDao = new UserFileDao(fileStorageEngine.getConnection());
-        users.forEach(userDao::create);
+        UserFileDao userDao = new UserFileDao(new FileStorageConnectionTest("."));
+
         userDao.delete(1);
-        userDao.get(2);
-        userDao.get(1);
+    }
+
+
+    /**
+     *
+     * Another design problem surfaces here: it would have been easier to implement StorageEngineConnection but we can't
+     * because UserFileDao depends on FileStorageConnection and not on the interface
+     */
+    private static class FileStorageConnectionTest extends FileStorageConnection {
+
+        public FileStorageConnectionTest(String dirPath) {
+            super(dirPath);
+        }
+
+        @Override
+        public Query createQuery(String queryAsString, Object... args) {
+            Assert.assertTrue(queryAsString.startsWith("d/") || queryAsString.startsWith("r/"));
+            if (queryAsString.startsWith("d/")) {
+                Assert.assertTrue(args[0].equals(1));
+            }
+            return new FileQueryGetTest();
+        }
+    }
+
+    private static class FileQueryGetTest implements  Query {
+
+        @Override
+        public byte[] execute() {
+            return (new User(1, "andrei", "aserea@fitbit.com", "Andrei", "Serea", "Home").toString() +
+                    "\n" + new User(2, "ion", "ion@fitbit.com", "Ion", "Popescu", "Home").toString() +
+                    FileQuery.TOMBSTONE + "\n" + 1).getBytes();
+        }
     }
 
 }
